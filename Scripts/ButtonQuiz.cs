0d022c3a19019a666714b9d6d46457d969407aac
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-14 09:06:27 +0100

    Update ButtonQuiz.cs

--- Scripts/ButtonQuiz.cs
+++ Scripts/ButtonQuiz.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonQuiz : MonoBehaviour
-{
-    public static void OnMouseOver()
-    {
+/** Pulsante specifico per far partire il minigioco del Quiz, gioco originale del team */
+public class ButtonQuiz : MonoBehaviour{
+    public static void OnMouseOver(){
         Menu.bQuizButon=true;
         Menu.bGamesMenu=false;
     }
-    public static void OnMouseExit()
-    {
+    public static void OnMouseExit(){
         Menu.bQuizButon=false;
         Menu.bGamesMenu=true;
     }
