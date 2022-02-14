b3e427d4979902fba2dcd98a4bee193d30cbf25d
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-14 09:08:11 +0100

    Update ButtonTris.cs

--- Scripts/ButtonTris.cs
+++ Scripts/ButtonTris.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonTris : MonoBehaviour
-{
-    public static void OnMouseOver()
-    {
+/** Bottone per far partire il minigioco specifico del tris */
+public class ButtonTris : MonoBehaviour{
+    public static void OnMouseOver(){
         Menu.bTrisButton=true;
         Menu.bGamesMenu=false;
     }
-    public static void OnMouseExit()
-    {
+    public static void OnMouseExit(){
         Menu.bTrisButton=false;
         Menu.bGamesMenu=true;
     }
