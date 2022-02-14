1de89b1083e069da34fa1b5e0d8bd3b82dbbccfc
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-14 08:58:02 +0100

    Update ButtonBack.cs

--- Scripts/ButtonBack.cs
+++ Scripts/ButtonBack.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonBack : MonoBehaviour
-{
-   public static void OnMouseOver()
-    {
+/** Controllo della puntatore sullo schermo */
+public class ButtonBack : MonoBehaviour{
+   public static void OnMouseOver(){
         Menu.bBackButton=true;
         Menu.bGamesMenu=false;
-    }
-    public static void OnMouseExit()
-    {
+   }
+    public static void OnMouseExit(){
         Menu.bBackButton=false;
         Menu.bGamesMenu=true;
     }
