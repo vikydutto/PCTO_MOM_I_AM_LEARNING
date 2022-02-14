26b960cc62bd292600eadfc321c0e2aacb4bcf1a
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-14 09:01:31 +0100

    Update ButtonForza4.cs

--- Scripts/ButtonForza4.cs
+++ Scripts/ButtonForza4.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonForza4 : MonoBehaviour
-{
-    public static void OnMouseOver()
-    {
+/** Bottone per entrare nel minigame di forza 4 */
+public class ButtonForza4 : MonoBehaviour{
+    public static void OnMouseOver(){
         Menu.bForza4Button=true;
         Menu.bGamesMenu=false;
     }
-    public static void OnMouseExit()
-    {
+    public static void OnMouseExit(){
         Menu.bForza4Button=false;
         Menu.bGamesMenu=true;
     }
