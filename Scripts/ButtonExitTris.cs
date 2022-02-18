e16aab257c5e159b68238d828c467ca4dbbdb7bc
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-18 13:38:45 +0100

    Update ButtonExitTris.cs

--- Scripts/ButtonExitTris.cs
+++ Scripts/ButtonExitTris.cs
@@ -1,16 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
 
 /** Bottone "Back"/"Indietro" generale */
-public class ButtonExitTris : MonoBehaviour
-{
-    public static void OnMouseOver() {
-        Tris.bPauseExit=true;
-        Tris.bPause=false;
+
+public class ButtonExitTris : MonoBehaviour{ // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public static void OnMouseOver() { // metodo per vedere se la mano è sopra all'oggetto del bottone per uscire dal gioco del Tris
+        Tris.bPauseExit = true; // la variabile diventa true -> cambia la schermata: torna al menù di pausa
+        Tris.bPause = false;
     }
-    public static void OnMouseExit() {
-        Tris.bPauseExit=false;
-        Tris.bPause=true;
+    public static void OnMouseExit() { // metodo per verificare se la mano non è sopra il button
+        Tris.bPauseExit = false;
+        Tris.bPause = true; // la scena continua a runnare
     }
 }
