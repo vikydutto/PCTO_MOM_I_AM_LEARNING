037158b9a1980e4c334338e348a491f8fff094d8
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-18 13:34:44 +0100

    Update ButtonBack.cs

--- Scripts/ButtonBack.cs
+++ Scripts/ButtonBack.cs
@@ -1,15 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
 
 /** Controllo della puntatore sullo schermo */
-public class ButtonBack : MonoBehaviour{
-   public static void OnMouseOver(){
-        Menu.bBackButton=true;
-        Menu.bGamesMenu=false;
+
+public class ButtonBack : MonoBehaviour{ // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+   public static void OnMouseOver(){ // metodo per vedere se la mano è sopra all'oggetto del bottone per andare indietro
+        Menu.bBackButton = true; // la variabile diventa true -> cambia la schermata
+        Menu.bGamesMenu = false;
    }
-    public static void OnMouseExit(){
-        Menu.bBackButton=false;
-        Menu.bGamesMenu=true;
+    public static void OnMouseExit(){ // metodo per verificare se la mano non è sopra il button
+        Menu.bBackButton = false;
+        Menu.bGamesMenu = true; // la variabile diventa true -> non cambia la schermata
     }
 }
