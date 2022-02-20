f9ef570aea14835ddd08f542e99a65cf13d15bfa
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 10:43:54 +0100

    Update ButtonForza4.cs

--- Scripts/ButtonForza4.cs
+++ Scripts/ButtonForza4.cs
@@ -1,15 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
 
 /** Bottone per entrare nel minigame di forza 4 */
-public class ButtonForza4 : MonoBehaviour{
-    public static void OnMouseOver(){
-        Menu.bForza4Button=true;
-        Menu.bGamesMenu=false;
+
+public class ButtonForza4 : MonoBehaviour { // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public static void OnMouseOver() { // metodo per vedere se la mano è sopra all'oggetto del bottone per iniziare a giocare a forza quattro: e quindi cambiare scena
+        Menu.bForza4Button = true; // si fa il "turn on" di questo bottone e quindi poi si cambia scena
+        Menu.bGamesMenu = false;
     }
-    public static void OnMouseExit(){
-        Menu.bForza4Button=false;
-        Menu.bGamesMenu=true;
+    public static void OnMouseExit() { // metodo per non cambiare scena
+        Menu.bForza4Button = false;
+        Menu.bGamesMenu = true; // la schermata non cambia: schermata del menù dei giochi
     }
 }
