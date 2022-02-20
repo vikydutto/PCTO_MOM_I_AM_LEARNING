c98890fd475ff7bae56a874221f3bc9d51e4d63a
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 22:25:23 +0100

    Update Menu.cs

--- Scripts/Menu.cs
+++ Scripts/Menu.cs
@@ -1,25 +1,23 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
+using UnityEngine.SceneManagement; // inclusione della libreria per controllare il movimento delle scene
 
-public class Menu : MonoBehaviour
-{
-    public GameObject MenuHome;
-    public GameObject OptionMenu,playButton,optionButton,RulesButton, QuizButton, TrisButton, Forza4Button, GamesMenu, BackButton;
-    public static bool bMenuHome,bOptionMenu,bplayButton,boptionButton,bRulesButton,bQuizButon, bTrisButton, bForza4Button, bGamesMenu, bBackButton;
+public class Menu : MonoBehaviour { // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public GameObject MenuHome; // menù principale: quello di inizio
+    public GameObject OptionMenu,playButton,optionButton,RulesButton, QuizButton, TrisButton, Forza4Button, GamesMenu, BackButton; // ogni bottone della fase 0 e 1
+    public static bool bMenuHome,bOptionMenu,bplayButton,boptionButton,bRulesButton,bQuizButon, bTrisButton, bForza4Button, bGamesMenu, bBackButton; // background(s)
 
-    void Start()
-    {
-       bMenuHome=true;
-       boptionButton=false;
-       bplayButton=false;
-       bOptionMenu=false;
-       bQuizButon=false;
-       bForza4Button=false;
-       bTrisButton=false;
-       bGamesMenu=false;
-       bBackButton=false;
+    void Start() { // all'inizio: quando si apre il gioco
+       bMenuHome = true; // si parte dal menù principale e quindi si setta il suo sfondo a vero
+       boptionButton = false;
+       bplayButton = false;
+       bOptionMenu = false;
+       bQuizButon = false;
+       bForza4Button = false;
+       bTrisButton = false;
+       bGamesMenu = false;
+       bBackButton = false; // mentre tutti gli altri a falso, dato che non sono delle scene che vogliamo
        MenuHome.SetActive(bMenuHome);
        OptionMenu.SetActive(bOptionMenu);
        playButton.SetActive(bplayButton); 
@@ -29,12 +27,11 @@ public class Menu : MonoBehaviour
        TrisButton.SetActive(bTrisButton);
        Forza4Button.SetActive(bForza4Button);
        GamesMenu.SetActive(bGamesMenu);
-       BackButton.SetActive(bBackButton);
+       BackButton.SetActive(bBackButton); // si settano ad attivi tutti i background
     }
 
-    void Update()
-    {
-        MenuHome.SetActive(bMenuHome);
+    void Update() { // all'update, che viene molte volte al secondo dato che avviene ad ogni frame
+       MenuHome.SetActive(bMenuHome);
        OptionMenu.SetActive(bOptionMenu);
        playButton.SetActive(bplayButton); 
        optionButton.SetActive(boptionButton);
@@ -43,6 +40,6 @@ public class Menu : MonoBehaviour
        TrisButton.SetActive(bTrisButton);
        Forza4Button.SetActive(bForza4Button);
        GamesMenu.SetActive(bGamesMenu);
-        BackButton.SetActive(bBackButton);
+       BackButton.SetActive(bBackButton); // si settano attivi tutti i background
     }
 }
