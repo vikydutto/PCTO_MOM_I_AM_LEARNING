ad29d870babeb574e94292dc7b3422508697a05d
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 12:02:59 +0100

    Update ButtonPlayTris.cs

--- Scripts/ButtonPlayTris.cs
+++ Scripts/ButtonPlayTris.cs
@@ -7,10 +7,10 @@ using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui 
 public class ButtonPlayTris : MonoBehaviour { // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
     public static void OnMouseOver() { // metodo per cui se il cursore è sul pulante farà:
         Tris.bPausePlay = true; // variabile che permette di giocare se messa true
-        Tris.bPause = false;
+        Tris.bPause = false; // impostata a false dato che ogni variabile ha un solo compito
     }
     public static void OnMouseExit() { // metodo per cui se il cursore NON è sul pulante farà:
-        Tris.bPausePlay = false;
+        Tris.bPausePlay = false; // impostata a false dato che ogni variabile ha un solo compito
         Tris.bPause = true; // resterà sulla schermata in cui è al momento
     }
 }
