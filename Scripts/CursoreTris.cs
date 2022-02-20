ab1ef86b7a0e1cac94288bff291039f36ae8efe5
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 15:23:58 +0100

    Update CursoreTris.cs

--- Scripts/CursoreTris.cs
+++ Scripts/CursoreTris.cs
@@ -1,43 +1,29 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
+using UnityEngine.UI; // header per la user interface
 
-public class CursoreTris : MonoBehaviour
-{
+/** Cursore del gioco del Tris */
+
+public class CursoreTris : MonoBehaviour { // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
     private float x;
-    private float y;
-    void Start()
-    {
+    private float y; // coordinate per la posizione della mano: float come sempre
+    
+    void Start(){
         y = gameObject.transform.position.y;
-        x = gameObject.transform.position.x;
+        x = gameObject.transform.position.x; // dall'UltraLeap alla posizione nella scena
     }
-    void Update()
-    {
+    
+    void Update(){ // update continuo della posizione della mano, come negli altri script dei cursori
         gameObject.transform.position = new Vector3(x, y, transform.position.z);
     }
 
-    public void setX(float x)
-    {
-        this.x = x;
-    }
-
-    public void setY(float y)
-    {
-        this.y = y;
-    }
-
-    public float getX()
-    {
-        return x;
-    }
-
-    public float getY()
-    {
-        return y;
-
-    }
-    public bool isOnQ(GameObject q)
+    public void setX(float x) this.x = x;
+    public void setY(float y) this.y = y;
+    public float getX() return x;
+    public float getY()return y; // setter e getter della mano
+    
+    public bool isOnQ(GameObject q) // metodo per capire se la mano è su uno dei 9 quadrati del mini-game
     {
         float width = q.GetComponent<RectTransform>().rect.width;
         float height = q.GetComponent<RectTransform>().rect.height;
