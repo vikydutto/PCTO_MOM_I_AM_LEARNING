81d0104eade9280ea261455550465a8c3acf8933
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 11:37:17 +0100

    Update ButtonOption.cs

--- Scripts/ButtonOption.cs
+++ Scripts/ButtonOption.cs
@@ -1,19 +1,19 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
 
 /** Pulsante per entrare nel menù opzioni */
-public class ButtonOption : MonoBehaviour{
-    public static void OnMouseOver() {
+public class ButtonOption : MonoBehaviour { // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public static void OnMouseOver() { // se il cursore è sul bottone delle opzioni la scena cambierà andando sulla shermata richiesta dall'utente
         /*Menu.bplayButton=true;
         Menu.bMenuHome=true;*/
-        Menu.boptionButton=true;
-        Menu.bMenuHome=false;
+        Menu.boptionButton = true; // se il cursore è sul bottone delle opzioni allora questa variabile sarà true e quindi poi cambierà scena di gioco
+        Menu.bMenuHome = false;
     }
-    public static void OnMouseExit() {
+    public static void OnMouseExit() { // se il mouse non è sul bottone questo metodo farà sì che
         /*Menu.bMenuHome=true;
         Menu.bplayButton=false;*/
-        Menu.boptionButton=false;
-        Menu.bMenuHome=true;
+        Menu.boptionButton = false;
+        Menu.bMenuHome = true; // questa variabile sia true e la boptionButton sia false così da non cambiare schermata di gioco
     }
 }
