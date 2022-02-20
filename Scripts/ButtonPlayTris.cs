3da24f9c8409f3eaef03d7ffc3d0a7b1a3be0730
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 11:54:33 +0100

    Update ButtonPlayTris.cs

--- Scripts/ButtonPlayTris.cs
+++ Scripts/ButtonPlayTris.cs
@@ -1,15 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
 
 /** Bottone specifico per far startare il gioco del Tris */
-public class ButtonPlayTris : MonoBehaviour{
-    public static void OnMouseOver() {
-        Tris.bPausePlay=true;
-        Tris.bPause=false;
+
+public class ButtonPlayTris : MonoBehaviour { // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public static void OnMouseOver() { // metodo per cui se il cursore è sul pulante farà:
+        Tris.bPausePlay = true; // variabile che permette di giocare se messa true
+        Tris.bPause = false;
     }
-    public static void OnMouseExit() {
-        Tris.bPausePlay=false;
-        Tris.bPause=true;
+    public static void OnMouseExit() { // metodo per cui se il cursore NON è sul pulante farà:
+        Tris.bPausePlay = false;
+        Tris.bPause = true; // resterà sulla schermata in cui è al momento
     }
 }
