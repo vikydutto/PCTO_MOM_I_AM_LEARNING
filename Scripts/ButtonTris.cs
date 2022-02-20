7a90fb3c31433e7a9159efaaf35387c53829196d
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 12:16:14 +0100

    Update ButtonTris.cs

--- Scripts/ButtonTris.cs
+++ Scripts/ButtonTris.cs
@@ -1,15 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
 
 /** Bottone per far partire il minigioco specifico del tris */
-public class ButtonTris : MonoBehaviour{
-    public static void OnMouseOver(){
-        Menu.bTrisButton=true;
-        Menu.bGamesMenu=false;
+
+public class ButtonTris : MonoBehaviour{ // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public static void OnMouseOver(){ // se il cursore è sopra il pulsante questo metodo viene invocato
+        Menu.bTrisButton = true; // questa variabile diventa true per cambiare scena e fare partire il gioco del tris
+        Menu.bGamesMenu = false; // questa viene messa a false per non tornare al menù principale
     }
-    public static void OnMouseExit(){
-        Menu.bTrisButton=false;
-        Menu.bGamesMenu=true;
+    public static void OnMouseExit(){ // metodo invocato quando il mouse non è sul pulsante
+        Menu.bTrisButton = false; // per non far artire il gioco
+        Menu.bGamesMenu = true; // per tornare alla schermata del menù principale
     }
 }
