67c5323cd97cfd08d9f988a73f8f17888f04ad67
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 22:38:12 +0100

    Update Risposta.cs

--- Scripts/Risposta.cs
+++ Scripts/Risposta.cs
@@ -1,22 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
+using TMPro; // header per utilizzare i font carivcati su unity dal programmatore: Text Mash Pro
+using UnityEngine.UI; // header per la user interface
 
-public class Risposta : MonoBehaviour
-{
-    public TextMeshProUGUI t;
-    public string testo;
-    void Start()
-    {
-        testo=":/";
-    }
+public class Risposta : MonoBehaviour { // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public TextMeshProUGUI t; // componente font
+    public string testo; // testo della risposta: la stringa
+    
+    void Start() testo =":/"; // testo a caso
 
-    void Update()
-    {
-        TextMeshPro p = t.GetComponent<TextMeshPro>();
-        t.text=testo;
-        
+    void Update() { // ogni volta che fa l'update
+        TextMeshPro p = t.GetComponent<TextMeshPro>(); // nuova variabile che contiene testo e font
+        t.text = testo; // si assegna il testo a t
     }
 }
