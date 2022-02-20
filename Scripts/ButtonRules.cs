fd90c7822aca258cc3222e0798923eaef5351cfb
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 12:01:59 +0100

    Update ButtonRules.cs

--- Scripts/ButtonRules.cs
+++ Scripts/ButtonRules.cs
@@ -1,19 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
 
-/** Pulsante  delle regole */
-public class ButtonRules : MonoBehaviour{
-    public static void OnMouseOver() {
+/** Pulsante delle regole */
+
+public class ButtonRules : MonoBehaviour{ // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public static void OnMouseOver() { // metodo che viene chiamato quando il cursore si posiziona sul pulsante delle regole
         /*Menu.bplayButton=true;
         Menu.bMenuHome=true;*/
-        Menu.bRulesButton=true;
-        Menu.bMenuHome=false;
+        Menu.bRulesButton = true; // si mette questa variabile a true per far passare alla schermata delle regole il gioco
+        Menu.bMenuHome = false; // questa la si imposta a false per non creare bug
     }
-    public static void OnMouseExit() {
+    public static void OnMouseExit() { // metodo chiamato quando il cursore NON è sul pulsante opzioni
         /*Menu.bMenuHome=true;
         Menu.bplayButton=false;*/
-        Menu.bRulesButton=false;
-        Menu.bMenuHome=true;
+        Menu.bRulesButton = false; // questa la si imposta a false per non creare bug
+        Menu.bMenuHome = true; // variabile che se impostata a true porta a far vedere la scena della home: schermata iniziale
     }
 }
