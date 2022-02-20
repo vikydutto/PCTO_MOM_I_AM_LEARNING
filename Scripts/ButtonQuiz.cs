6e37a2a122bc1f8330d28e7de57cbe734d5e45b5
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 11:58:17 +0100

    Update ButtonQuiz.cs

--- Scripts/ButtonQuiz.cs
+++ Scripts/ButtonQuiz.cs
@@ -1,15 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
 
 /** Pulsante specifico per far partire il minigioco del Quiz, gioco originale del team */
-public class ButtonQuiz : MonoBehaviour{
-    public static void OnMouseOver(){
-        Menu.bQuizButon=true;
-        Menu.bGamesMenu=false;
+
+public class ButtonQuiz : MonoBehaviour{ // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public static void OnMouseOver(){ // metodo che viene chiamato quando il mouse è sul bottone apposito
+        Menu.bQuizButon = true; // se questo metodo viene chiamato questa variabile viene impostata a true per far partire il gioco del quiz
+        Menu.bGamesMenu = false; // questa viene messa a false per non creare bugs
     }
-    public static void OnMouseExit(){
-        Menu.bQuizButon=false;
-        Menu.bGamesMenu=true;
+    public static void OnMouseExit(){ // metodo che viene chiamato quando il mouse non è sul pulsante per far "startare" il minigioco main della startup
+        Menu.bQuizButon = false; // questa variabile viene messa a false così che non continui con la scena del quiz
+        Menu.bGamesMenu = true; // resta sulla schermata della scelta dei giochi
     }
 }
