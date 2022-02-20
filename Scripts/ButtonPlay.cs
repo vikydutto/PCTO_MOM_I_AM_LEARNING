adbe2f1c484c836438bea28b11a17717760f4f02
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 11:49:03 +0100

    Update ButtonPlay.cs

--- Scripts/ButtonPlay.cs
+++ Scripts/ButtonPlay.cs
@@ -1,19 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
 
 /** Pulsante in generale per far startare il minigioco, qualsiasi esso sia */
-public class ButtonPlay : MonoBehaviour{
-    public static void OnMouseOver() {
+
+public class ButtonPlay : MonoBehaviour { // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public static void OnMouseOver() { // questo metodo quando il mouse arriva sul pulsante fa si che
         /*Menu.bplayButton=true;
         Menu.bMenuHome=true;*/
-        Menu.bplayButton=true;
-        Menu.bMenuHome=false;
+        Menu.bplayButton = true; // questa variabile diventi true per cambiare scena e quella successiva per non creare bugs
+        Menu.bMenuHome = false;
     }
-    public static void OnMouseExit() {
+    public static void OnMouseExit() { // se il mouse non è sul pulsante
         /*Menu.bMenuHome=true;
         Menu.bplayButton=false;*/
-        Menu.bplayButton=false;
-        Menu.bMenuHome=true;
+        Menu.bplayButton = false;
+        Menu.bMenuHome = true; // variabile che diventa true per restare alla schermata del menù home, ovvero quella iniziale
     }
 }
