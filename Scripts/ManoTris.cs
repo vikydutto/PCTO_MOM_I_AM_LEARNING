ceefae049be029584a4ef4fe7436718ecc4a47a3
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-20 22:20:17 +0100

    Update ManoTris.cs

--- Scripts/ManoTris.cs
+++ Scripts/ManoTris.cs
@@ -1,33 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEngine;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
+using UnityEngine.UI; // header per la user interface
 
-public class ManoTris : MonoBehaviour
-{
-    public GameObject palmo;
-    public GameObject dito;
-    private float sx,sy,sz,dx,dy,dz;
-    public CursoreTris cursore;
+public class ManoTris : MonoBehaviour {  // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public GameObject palmo, dito; // palmo e dito per la mano nel mini gioco del Tris
+    private float sx,sy,sz, dx,dy,dz; // coordinate del palmo nella realtà: rilevate dall'UltraLeap
+    public CursoreTris cursore; // il cursore della classe CursoreTris
     private float x;
-    private float y;
-    void Update()
-    {
+    private float y; // coordinate del cursore
+    // stessi attributi per anche le altre mani
+    
+    void Update() {
         sx=palmo.transform.position.x;
         sy=palmo.transform.position.y;
-        sz=palmo.transform.position.z;
+        sz=palmo.transform.position.z; // coordinate del palmo
         dx=dito.transform.position.x;
         dy=dito.transform.position.y;
-        dz=dito.transform.position.z;
+        dz=dito.transform.position.z; // coordinate del dito medio
 
-        x= ((sx+75.0f)*2400/450.0f)-130;
-        y= (sy+40)*1500/220-140;
+        x= ((sx + 75.0f)*2400/450.0f)-130;
+        y= (sy + 40)*1500/220-140; // numeri per regolazre posizione e velocità cursore nel gioco del tris
         cursore.setX(x);
-        cursore.setY(y);
+        cursore.setY(y); // si passano i valori al cursore
 
     }
 
-    public bool isManoChiusa(){
-        return Mathf.Sqrt((sx-dx)*(sx-dx)+(sy-dy)*(sy-dy)+(sz-dz)*(sz-dz))<10;
-    }
+    public bool isManoChiusa() return Mathf.Sqrt((sx-dx)*(sx-dx)+(sy-dy)*(sy-dy)+(sz-dz)*(sz-dz))<10; // metodo per rilevare se la mano è chiusa
 }
