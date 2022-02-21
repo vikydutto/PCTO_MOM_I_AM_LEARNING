ede8903e4e2b030ecc5cb45ed41da552037b61b0
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2022-02-21 01:06:56 +0100

    Update explosion.cs

--- Scripts/explosion.cs
+++ Scripts/explosion.cs
@@ -1,26 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections.Generic; // 2 headers scritte di default per utilizzare Unity
+using UnityEngine; // utilizzata per accesso ad accelerometro e multi-touch sui devices
 
-public class explosion : MonoBehaviour
-{
-    public static GameObject sfondo;
-    public GameObject expGreen;
-    public GameObject expRed;
-    public static bool right = false;
-    void OnCollisionEnter()
-    {
-        if (explosion.right)
-        {
-            GameObject p = Instantiate(expGreen, transform.position, transform.rotation);
-            p.transform.SetParent(sfondo.transform, false);
+/** Classe che controlla l'esplosione delle parti dietro la scena del gioco del quiz */
+
+public class explosion : MonoBehaviour { // MonoBehaviour: la classe da cui tutti gli script derivano in Unity
+    public static GameObject sfondo; // sfondo della scena
+    public GameObject expGreen, expRed; // esplosione verde e rossa in base alla correttezza della risposta dell'utente
+    public static bool right = false; // booleana per la risposta, vera o scorretta
+    
+    void OnCollisionEnter() { // se la pallina arriva e tocca l'oggetto dietro al quadrato della risposta data
+        if (explosion.right) { // se la risposta data è corretta
+            GameObject p = Instantiate(expGreen, transform.position, transform.rotation); // pallina fa esplodere di verde l'oggetto dietro lo sfondo del quiz
+        } else { // se la risposta data NON è corretta
+            GameObject p = Instantiate(expRed, transform.position, transform.rotation); // pallina fa esplodere di rosso l'oggetto dietro lo sfondo del quiz
         }
-        else
-        {
-            GameObject p = Instantiate(expRed, transform.position, transform.rotation);
-            p.transform.SetParent(sfondo.transform, false);
-        }
-        Destroy(gameObject);
+        p.transform.SetParent(sfondo.transform, false); // la pallina adesso è come se non avesse una classe genitore
+        Destroy(gameObject); // distruzione dell'oggetto p 
     }
 }
 
